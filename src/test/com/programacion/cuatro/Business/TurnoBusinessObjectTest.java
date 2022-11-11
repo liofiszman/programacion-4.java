2585a337150ba0a49604e4066ddd9d40a577e7f4
liofiszman <71087558+liofiszman@users.noreply.github.com>
2022-11-10 21:30:44 -0300

    Added JUNIT AND EXAMPLE

---
+++ src/test/com/programacion/cuatro/Business/TurnoBusinessObjectTest.java
@@ -0,0 +1,19 @@
+package com.programacion.cuatro.Business;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TurnoBusinessObjectTest {
+
+    @Test
+    void obtenerTurnos() {
+    }
+
+    @Test
+    void testObtenerTurnos() {
+    }
+
+    @Test
+    void registrarActividades() {
+    }
+}
